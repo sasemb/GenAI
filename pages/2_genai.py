adcbe4bc45de48ddd5ac6eae8a6bdf1c9315d6f6
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 17:02:13 +0100

    Update 2_genai.py

--- pages/2_genai.py
+++ pages/2_genai.py
@@ -29,35 +29,36 @@ if option == "Upload":
 elif option == "Selfie":
     st.text("Got no picture? Don't worry , Strike a pose and let's selfie")
     image = st.camera_input("Take a picture")
-img1 = Image.open(image)
-img1 = img1.resize((256, 256))
-answers = stability_api.generate("Convert the uploaded image of a person into a realistic pencil drawing sketch with a strong emphasis on capturing the facial features accurately. Ensure that the sketch highlights the key facial elements such as the eyes, nose, mouth, and overall facial structure. Pay attention to shading and details to create a lifelike pencil sketch. Please provide a high-quality sketch with attention to fine lines, shading, and proportions. The output should maintain the likeness of the original photograph while simulating a hand-drawn pencil sketch effect. Use your artistic judgment to create a visually appealing sketch.",
-    init_image=img1, 
-    start_schedule=0.6, 
-    seed=12345,
-    steps=50,
-    cfg_scale=7, 
-    width=256,
-    height=256, # Generation height, defaults to 512 if not included.
-    sampler=generation.SAMPLER_K_DPMPP_2M 
-)
-for resp in answers:
-        for artifact in resp.artifacts:
-            if artifact.finish_reason == generation.FILTER:
-                warnings.warn(
-                    "Your request activated the API's safety filters and could not be processed."
-                    "Please modify the prompt and try again.")
-            if artifact.type == generation.ARTIFACT_IMAGE:
-                img2 = Image.open(io.BytesIO(artifact.binary)) # Set our resulting initial image generation as 'img2' to avoid overwriting our previous 'img' generation.
-                # st.image(img2, width=256)
-                image_comparison(
-                    img1=img1,
-                    img2=img2,
-                    label1="Original",
-                    label2="Gen AI",
-                    width=256,
-                    starting_position=50,
-                    show_labels=True,
-                    make_responsive=True,
-                    in_memory=True,
-                )
+if image is not None:
+    img1 = Image.open(image)
+    img1 = img1.resize((256, 256))
+    answers = stability_api.generate("Convert the uploaded image of a person into a realistic pencil drawing sketch with a strong emphasis on capturing the facial features accurately. Ensure that the sketch highlights the key facial elements such as the eyes, nose, mouth, and overall facial structure. Pay attention to shading and details to create a lifelike pencil sketch. Please provide a high-quality sketch with attention to fine lines, shading, and proportions. The output should maintain the likeness of the original photograph while simulating a hand-drawn pencil sketch effect. Use your artistic judgment to create a visually appealing sketch.",
+        init_image=img1, 
+        start_schedule=0.6, 
+        seed=12345,
+        steps=50,
+        cfg_scale=7, 
+        width=256,
+        height=256, # Generation height, defaults to 512 if not included.
+        sampler=generation.SAMPLER_K_DPMPP_2M 
+    )
+    for resp in answers:
+            for artifact in resp.artifacts:
+                if artifact.finish_reason == generation.FILTER:
+                    warnings.warn(
+                        "Your request activated the API's safety filters and could not be processed."
+                        "Please modify the prompt and try again.")
+                if artifact.type == generation.ARTIFACT_IMAGE:
+                    img2 = Image.open(io.BytesIO(artifact.binary)) # Set our resulting initial image generation as 'img2' to avoid overwriting our previous 'img' generation.
+                    # st.image(img2, width=256)
+                    image_comparison(
+                        img1=img1,
+                        img2=img2,
+                        label1="Original",
+                        label2="Gen AI",
+                        width=256,
+                        starting_position=50,
+                        show_labels=True,
+                        make_responsive=True,
+                        in_memory=True,
+                    )
