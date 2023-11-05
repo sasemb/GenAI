06b4a65a86fdf46bcedd920ca62e3de7a5894041
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 12:49:00 +0100

    Update 1_page.py

--- 1_page.py
+++ 1_page.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
-import numpy as np
 st.set_page_config(
   page_title="From Selfie to Sketchie", 
   page_icon="🤩", 
