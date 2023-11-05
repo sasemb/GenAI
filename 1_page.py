42a27db99a0c5c2bf336724c420c27a250117cf4
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 15:11:10 +0100

    Update 1_page.py

--- 1_page.py
+++ 1_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
-#from st_pages import hide_pages
+from st_pages import add_page_title, hide_pages
+add_page_title()
 st.set_page_config(
   page_title="From Selfie to Sketchie", 
   page_icon="🤩", 
@@ -15,7 +16,7 @@ st.set_page_config(
 st.title('From Selfie to :rainbow[Sketchie] ')
 st.subheader("Transform Your beautiful selfies to sketchies🤩")
 st.image('mp.jpg' , width=256)
-#hide_pages(["pages/2_genai"])
+hide_pages(["2_genai"])
 st.sidebar.success("Select a payment option.")
 stripe_js = """<script async
   src="https://js.stripe.com/v3/buy-button.js">
