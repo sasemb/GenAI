c3585726028041aa552277a649fddadf8f1d2d65
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 17:34:28 +0100

    Update 2_genai.py

--- pages/2_genai.py
+++ pages/2_genai.py
@@ -17,7 +17,7 @@ stability_api = client.StabilityInference(
 
 st.set_page_config(page_title="Sketches", page_icon=":)", layout="centered", initial_sidebar_state="collapsed")
 
-st.title("It's time for a change")
+st.title("Discover which 'Friends' movie character you'll become")
 
 option = st.selectbox(
    "Upload a image or Selfie?",
@@ -34,7 +34,7 @@ time.sleep(15)
 if image is not None:
     img1 = Image.open(image)
     img1 = img1.resize((256, 256))
-    answers = stability_api.generate("Transform the image into a stable diffusion black and white sketch with a focus on maintaining image stability and enhancing the sketch-like quality. Emphasize contrast and fine details while preserving the overall integrity of the original image. Ensure that the output resembles a high-quality black and white pencil sketch with smooth transitions and accurate representation of the original scene. Pay particular attention to achieving a stable diffusion effect, resulting in a visually appealing and realistic black and white sketch.",
+    answers = stability_api.generate("Transform the uploaded image into a recognizable 'Friends' TV show character using stable diffusion. Identify the key facial features and unique characteristics of the chosen character, and apply a stylized effect to achieve a likeness to the character. Ensure that the output resembles the selected 'Friends' character, capturing their iconic appearance and personality traits. Pay special attention to facial expressions, hairstyles, and any distinctive wardrobe elements that define the character's appearance. The result should be a convincing and well-executed representation of the chosen 'Friends' character, using stable diffusion for a visually appealing effect",
         init_image=img1, 
         start_schedule=0.6, 
         seed=12345,
