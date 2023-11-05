6bb9e4e7fe95a6f8361ead1a69190679c619e3e4
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 18:05:55 +0100

    Update 1_page.py

--- 1_page.py
+++ 1_page.py
@@ -4,35 +4,8 @@ from streamlit.source_util import _on_pages_changed, get_pages
 from pathlib import Path
 #from st_pages import show_pages, Page, hide_pages
 DEFAULT_PAGE = "1_page.py"
-def get_all_pages():
-    default_pages = get_pages(DEFAULT_PAGE)
-
-    pages_path = Path("pages.json")
-
-    if pages_path.exists():
-        saved_default_pages = json.loads(pages_path.read_text())
-    else:
-        saved_default_pages = default_pages.copy()
-        pages_path.write_text(json.dumps(default_pages, indent=4))
-
-    return saved_default_pages
-
-
-def clear_all_but_first_page():
-    current_pages = get_pages(DEFAULT_PAGE)
-
-    if len(current_pages.keys()) == 1:
-        return
-
-    get_all_pages()
-
-    # Remove all but the first page
-    key, val = list(current_pages.items())[0]
-    current_pages.clear()
-    current_pages[key] = val
-
-    _on_pages_changed.send()
-clear_all_but_first_page()
+current_pages = "2_genai.py"
+current_pages.clear()
 st.set_page_config(
   page_title="From Selfie to Sketchie", 
   page_icon="🤩", 
