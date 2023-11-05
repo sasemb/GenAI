ff84eb7a6a26a2c393b9c8b9d411a291c57c698a
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 11:23:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
-st.set_page_config(
-  page_title="From Selfie to Sketchie", 
-  page_icon="🤩", 
-  layout="centered", 
-  initial_sidebar_state="collapsed",
-  menu_items={
-        'Get Help': 'https://github.com/sasemb',
-        'Report a bug': "https://github.com/sasemb",
-        'About': "# Cool app to transform your selfies into sketchies!"
-    }
-)
+st.set_page_config(page_title="Pixar Star", page_icon="⭐", layout="centered", initial_sidebar_state="collapsed")
 st.title('From Selfie to Sketchie!')
 st.subheader("Transform Your beautiful selfies to sketchies ")
