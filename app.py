4648c80e54a3a06583754d293ed3d69b242d3c74
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-05 11:15:45 +0100

    Update config changes to streamlit

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+st.set_page_config(
+  page_title="From Selfie to Sketchie", 
+  page_icon="🤩", 
+  layout="centered", 
+  initial_sidebar_state="collapsed"
+  menu_items={
+        'Get Help': 'https://github.com/sasemb',
+        'Report a bug': "https://github.com/sasemb",
+        'About': "# Cool app to transform your selfies into sketchies!"
+    }
+)
 st.title('Transform into Your Inner Superhero!')
 st.subheader("Transform Your beautiful images into Superheroes with superpowers")
