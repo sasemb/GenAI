a3d95c9f12659052fc4aac558556d7fe7471b687
sasemb <31820752+sasemb@users.noreply.github.com>
2023-11-08 14:34:14 +0100

    Update 1_page.py

--- 1_page.py
+++ 1_page.py
@@ -2,10 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 from streamlit.source_util import _on_pages_changed, get_pages
 from pathlib import Path
-#from st_pages import show_pages, Page, hide_pages
-#DEFAULT_PAGE = "1_page.py"
-#current_pages = "pages/2_genai.py"
-#current_pages.clear()
+
 st.set_page_config(
   page_title="From Selfie to Sketchie", 
   page_icon="🤩", 
@@ -31,7 +28,4 @@ stripe_js = """<script async
 """.format(st.secrets["publishable_key"])
 
 
-#picture = st.camera_input("No need to fret if you don't already have a photo—snap a selfie right away!")
-#if picture:
-#    st.image(picture)
 components.html(html=stripe_js, height=300)
